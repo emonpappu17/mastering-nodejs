7f927b6df3f40ccd24791b106729eb87b6504f5f
emonpappu17 <emon.pappu.5@gmail.com>
2025-06-17 00:02:10 +0600

    add progress

--- mongoose-module/src/app/models/user.model.ts
+++ mongoose-module/src/app/models/user.model.ts
@@ -1,21 +1,44 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
+import validator from 'validator'
 
 const userSchema = new Schema<IUser>({
     firstName: {
         type: String,
-        required: true,
-        trim: true
+        required: [true, "firstName keno dao nay?"],
+        trim: true,
+        minlength: [5, "FirstName must be 5 characters, got {VALUE}"],
+        maxlength: 10
     },
     lastName: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        minlength: [5, "LastName must be 5 characters"],
+        maxlength: 10
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: [18, 'Age Must be at least 18, got {VALUE}'],
+        max: 60
     },
     email: {
         type: String,
+        unique: [true, "Email common hoye geche!!"],
         required: true,
-        trim: true
+        lowercase: true,
+        trim: true,
+        // validate: {
+        //     validator: function (value) {
+        //         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+        //         // return false
+        //     },
+        //     message: function (props) {
+        //         return `Email ${props.value} is not valid email`
+        //     }
+        // }
+        validate: [validator.isEmail, "Invalid Email sent {VALUE}"]
     },
     password: {
         type: String,
@@ -23,8 +46,13 @@ const userSchema = new Schema<IUser>({
     },
     role: {
         type: String,
-        enum: ['user', 'admin'],
-        default: 'user'
+        uppercase: true,
+        // enum: ['USER', 'ADMIN', 'SUPERADMIN'],
+        enum: {
+            values: ['USER', 'ADMIN', 'SUPERADMIN'],
+            message: "Role is not valid, got {VALUE} role"
+        },
+        default: 'USER'
     }
 })
 
